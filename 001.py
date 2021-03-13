46d038d1d303c3fea0d8cd1190a1e2b5319bd21a
ZerafCode <ZerafCode@gmail.com>
2021-03-13 20:16:11 +0300

    welcome-to python self study

---
+++ 001.py
@@ -0,0 +1,5 @@
+# python for beginners
+print('<<--------------------------->>')
+print('----welcome to python-3--------')
+print('------self study-----2021------')
+print('<<___________________________>>')
