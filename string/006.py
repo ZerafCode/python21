d020f81385be9e654c058932a9cdc4d37f349d80
ZerafCode <ZerafCode@gmail.com>
2021-03-18 15:53:45 +0300

    string  concatenate exampls

--- string/006.py
+++ string/006.py
@@ -7,10 +7,16 @@ print(new_message)
 print('%s %s' % (name, message))
 print('%s, %s! ' % (name, message))
 print('<< %s, %s! >>' % (name, message))
+print("%s %s %s %s " % ("welcome", "to", "python", "Hub"))
 number = 3
 print('<< ' + name + ',😍 ' + (message) + str(3) + ' 👌')
 print(f'{name}, {message} 3')
 print('{} {}.'.format(name, message))
+print(name + " " + message)
 name += ', ' + message
 print(name)
-
+list_message = ['welcome', 'to', 'python3']
+empty_string = ' '
+for lists in list_message:
+    empty_string += lists
+print(empty_string)
