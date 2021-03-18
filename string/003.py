8cd22e08b2163f4a7d92b7034827fb4e92f16850
ZerafCode <ZerafCode@gmail.com>
2021-03-18 12:47:55 +0300

    string

---
+++ string/003.py
@@ -0,0 +1,9 @@
+message = 'welcome to python self study!!'
+print(message)
+message = "welcome to python self study"
+print(message)
+message = "welcome to python's self study"
+print(message)
+message = 'welcome to "python" self study'
+print(message)
+
